6c62767b5c6b461723c0b2c389c9c43979ccb24c
Vishesh Verma <99197015+VISHESH0932@users.noreply.github.com>
2025-05-24 11:29:17 +0530

    Updated  settlemint2.cpp

--- settlemint2.cpp
+++ settlemint2.cpp
@@ -1,45 +1,28 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-
-int countLightSwitches(int n, const vector<pair<int, int>>& intervals) {
-    vector<pair<int, int>> events;
-
-    for (const auto& interval : intervals) {
-        events.emplace_back(interval.first, 1);   
-        events.emplace_back(interval.second, -1); 
-    }
-
-    sort(events.begin(), events.end());
-
-    int inside = 0;
-    int switchCount = 0;
-
-    for (const auto& event : events) {
-        int time = event.first;
-        int delta = event.second;
-
-        if (delta == 1 && inside == 0) {
-            switchCount++; 
-        }
-
-        inside += delta;
-    }
-
-    return switchCount;
-}
-
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
     int n;
     cin >> n;
-
-    vector<pair<int, int>> intervals(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> intervals[i].first >> intervals[i].second;
+    vector<pair<long long,int>> events;
+    for(int i = 0; i < n; ++i) {
+        long long a, b;
+        cin >> a >> b;
+        events.emplace_back(a, +1);
+        events.emplace_back(b, -1);
     }
-
-    cout << countLightSwitches(n, intervals) << endl;
-
+    sort(events.begin(), events.end());
+    long long occupancy = 0;
+    long long switches = 0;
+    for(auto &e : events) {
+        if(e.second == +1) {
+            if(occupancy == 0) switches++;
+            occupancy++;
+        } else {
+            occupancy--;
+        }
+    }
+    cout << switches;
     return 0;
 }
